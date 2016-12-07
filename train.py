66660abef4735f42545709f995481353baebb37d
JesseYang <jesse.yang1985@gmail.com>
2016-12-07 10:42:38 +0800

    u

--- train.py
+++ train.py
@@ -13,12 +13,12 @@ from model import SegModel
 
 BATCH_SIZE = 4
 NUM_STEPS = 10000
-LEARNING_RATE = 0.0005
+LEARNING_RATE = 0.005
 KLASS = 6
 INPUT_CHANNEL = 3
 LOGDIR_ROOT = './logdir'
 STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
-SEG_PARAMS = './seg_params.json'
+SEG_PARAMS = './owl_seg_params.json'
 TRAINING_SET_DIR = './owl_data_training_set/'
 L2_REGULARIZATION_STRENGTH = 0
 
